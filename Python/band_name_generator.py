6bf2cb195303dfdd21252ddb5e69a232fd9cbf01
JoaoBett <joao.gouveia.bettencourt@gmail.com>
2023-05-17 16:31:26 +0100

    feat: day1 BandNameGenerator

---
+++ Python/band_name_generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Generator.\n")
+
+name=input("What's the name of the city you grew up in?\n")
+
+pet_name=input("What's your pet's name?\n")
+
+print("Your band name could be " + name + pet_name)
