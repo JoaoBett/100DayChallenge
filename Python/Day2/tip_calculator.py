cffe466aa967681f9475ba6157163f5108947fac
JoaoBett <joao.gouveia.bettencourt@gmail.com>
2023-05-18 00:32:38 +0100

    feat: day2 TipCalculator

--- Python/Day2/tip_calculator.py
+++ Python/Day2/tip_calculator.py
@@ -1,15 +1,15 @@
 print("Welcome to the tip calculator.")
 
-total_bill=float(input("What was the total bill?\n$"))
-people_split=int(input("How many people to split the bill?\n"))
+initial_bill=float(input("What was the total bill?\n-->$"))
+tip_percentage=int(input("What percentage tip would you like to give?(10%,12% or 15%)\n-->"))
+people_split=int(input("How many people to split the bill?\n-->"))
 
-bill_person=total_bill/people_split
-
-tip_percentage=int(input("What percentage tip would you like to give?(10%,12'%' or 15%)"))
 tip_percentage=tip_percentage/100
 
-final_payment=bill_person+(tip_percentage*bill_person)
+tip_ammount=initial_bill*tip_percentage
 
-print("Each person should pay: " + str(final_payment))
+final_bill=round((initial_bill+tip_ammount)/people_split, 2)
+
+print("Each person should pay: " + str(final_bill))
 
 input("Press to continue...")
