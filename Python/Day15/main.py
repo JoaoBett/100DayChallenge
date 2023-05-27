10ebe2c18332fa34b9181c893e4933584edad735
JoaoBett <joao.gouveia.bettencourt@gmail.com>
2023-05-27 23:29:59 +0100

    feat: start coffee POO

--- Python/Day15/main.py
+++ Python/Day15/main.py
@@ -1,3 +1,8 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+
+while #machine_working:
+    coffee_choice=input("What would you like?")
