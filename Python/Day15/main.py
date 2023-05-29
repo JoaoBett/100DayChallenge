438c88c0998e4f4b732db31a271a1729ef82b076
JoaoBett <joao.gouveia.bettencourt@gmail.com>
2023-05-29 22:09:12 +0100

    feat: day15 POO_coffee_machine

--- Python/Day15/main.py
+++ Python/Day15/main.py
@@ -1,8 +1,24 @@
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+machine_working = True
+money_machine = MoneyMachine()
+menu = Menu()
+coffee_maker = CoffeeMaker()
 
+while machine_working:
+    options = menu.get_items()
+    coffee_choice=input("What would you like? (espresso, latte, cappuccino): ").lower()
 
-while #machine_working:
-    coffee_choice=input("What would you like?")
+    if coffee_choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    elif coffee_choice == "off":
+        print("Powering off")
+        machine_working = False
+    else:
+        drink = menu.find_drink(coffee_choice)
+
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
