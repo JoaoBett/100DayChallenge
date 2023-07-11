4efbee8765dee6d8732a0d368fcac53ce121ed8c
JoaoBett <joao.gouveia.bettencourt@gmail.com>
2023-07-11 13:39:15 +0100

    feat: day17 HirstPainting

--- Python/Day17/main.py
+++ Python/Day17/main.py
@@ -3,13 +3,22 @@ import random
 
 tim = t.Turtle()
 
-colours = ["IndianRed","wheat"]
 directions = [0,90,180,270]
+tim.pensize(15)
+tim.speed("fastest")
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
 
 for _ in range(200):
+    tim.color(random_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))
-    tim.color(random.choice(colours))
-
+    
 
 t.done()
