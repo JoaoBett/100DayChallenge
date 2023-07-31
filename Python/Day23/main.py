32a4054ffc041d8239b61b454355cf517ac92a8d
JoaoBett <joao.gouveia.bettencourt@gmail.com>
2023-07-31 23:45:57 +0100

    feat: dataframes

--- Python/Day23/main.py
+++ Python/Day23/main.py
@@ -14,5 +14,40 @@ input("")
 import pandas as pd
 
 data = pd.read_csv("weather_data.csv")
-print(data["temp"])
+#print(data["temp"])
 input("")
+
+data_dict = data.to_dict()
+#print(data_dict)
+
+temp_list = data["temp"].to_list()
+#print(temp_list)
+
+average = sum(temp_list)/len(temp_list)
+
+data["temp"].mean()
+
+data["temp"].max()
+
+#get data in columns
+#print(data["codition"])
+data.condition
+
+#get data in row
+#print(data[data.day == "Monday"])
+
+#print(data[data.temp == data["temp"].max()])
+
+monday = data[data.day == "Monday"]
+print(monday.condition)
+
+monday_temp = int(monday.temp)
+monday_temp_F = monday_temp * 9/5 + 32
+
+
+data_dict = {
+    "students": ["Amy","James","Angela"],
+    "scores": [76, 56, 65]
+}
+data = pd.DataFrame(data_dict)
+data.to_csv("new_data.csv")
