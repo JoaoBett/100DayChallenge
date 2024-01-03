676794ab5f0015cde794e60cf7bde661c3876653
Joao Bett <joao.gouveia.bettencourt@gmail.com>
2024-01-03 02:05:26 +0000

    feat: day026

--- Python/Day026/kilometer.py
+++ Python/Day026/kilometer.py
@@ -1,12 +1,31 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+def miles_to_km():
+    miles = float(miles_input.get())
+    km = miles * 1.609
+    km_result.config(text=f"{km}")
 
-window.title("Kilometer to Miles")
-window.minsize(width=500, height=300)
+window = Tk()
+window.title("Miles to Kilometer")
+window.minsize(width=250, height=100)
 
 
-km_label = tkinter.Label("Kilometer:")
-int(input(""))
+miles_input = Entry(width=7)
+miles_input.grid(column=1 ,row=0)
+
+miles_label = Label(text="Miles")
+miles_label.grid(column=2 ,row=0)
+
+is_equal_to = Label(text="is equal to")
+is_equal_to.grid(column=0 ,row=1)
+
+km_result = Label(text="0")
+km_result.grid(column=1 ,row=1)
+
+km_label = Label(text="Km:")
+km_label.grid(column=2 ,row=1)
+
+calculate_button = Button(text="Calculate", command=miles_to_km)
+calculate_button.grid(column=1 ,row=2)
 
 window.mainloop()
