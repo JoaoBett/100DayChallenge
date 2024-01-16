ed6aed1abc68d5e769048210a2b72245dae2eec4
JoaoBett <joao.gouveia.bettencourt@gmail.com>
2024-01-16 20:56:18 +0000

    feat: Day033 - send-SMS

--- Python/Day033/challenge.py
+++ Python/Day033/challenge.py
@@ -1,28 +1,33 @@
 import requests
+from twilio.rest import Client
 
-MY_LAT = 39.749535
-MY_LONG = -8.807683
-
-OMW_ENDPOINT = "api_name"
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 api_key = "api_key"
+account_sid = "account_id"
+auth_token = "auth_token"
 
 weather_params = {
-    "lat" : MY_LAT,
-    "long" : MY_LONG,
-    "api": "api_key",
-    "cnt": 4
+    "lat": 46.947975,
+    "lon": 7.447447,
+    "appid": api_key,
+    "cnt": 4,
 }
 
-response = requests.get(OMW_ENDPOINT, params=weather_params)
+response = requests.get(OWM_Endpoint, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
-#print(weather_data["list"][0]["weather"][0][id])
-will_rain = False
 
+will_rain = False
 for hour_data in weather_data["list"]:
     condition_code = hour_data["weather"][0]["id"]
     if int(condition_code) < 700:
         will_rain = True
-
 if will_rain:
-    print("Bring umbrella")
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an ☔️",
+        from_="YOUR TWILIO VIRTUAL NUMBER",
+        to="YOUR TWILIO VERIFIED REAL NUMBER"
+    )
+    print(message.status)
